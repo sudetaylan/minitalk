532330e574a3b14a767a82931178f00b9e26deae
sudetaylan <157472956+sudetaylan@users.noreply.github.com>
2025-03-22 23:39:43 +0300

    Update client.c

--- client.c
+++ client.c
@@ -10,77 +10,93 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include <unistd.h>
+#include "minitalk.h"
 
-static int	g_ack_received;
+static int g_ack_received;
 
-void	ack_handler(int signo)
+void ack_handler(int signo)
 {
-	(void)signo;
-	g_ack_received = 1;
+    (void)signo;
+    g_ack_received = 1;
 }
 
-static int	ft_atoi(const char *str)
+static int ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	result;
-
-	i = 0;
-	sign = 1;
-	result = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i++;
-	if (str[i] == 43 || str[i] == 45)
-	{
-		if (str[i] == 45)
-			sign = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + str[i] - 48;
-		i++;
-	}
-	return (sign * result);
+    int i;
+    int sign;
+    int result;
+    
+    i = 0;
+    sign = 1;
+    result = 0;
+    while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+        i++;
+    if (str[i] == 43 || str[i] == 45)
+    {
+        if (str[i] == 45)
+            sign = -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + str[i] - 48;
+        i++;
+    }
+    return (sign * result);
 }
 
-void	send_char(int server_pid, char c)
+void send_char(int server_pid, char c)
 {
-	struct sigaction	sa;
-	int					i;
-
-	i = 0;
-	sa.sa_handler = ack_handler;
-	sa.sa_flags = 0;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
-	while (i < 8 && !g_ack_received)
-	{
-		g_ack_received = 0;
-		if (c & (1 << i))
-			kill(server_pid, SIGUSR1);
-		else
-			kill(server_pid, SIGUSR2);
-		pause();
-		i++;
-	}
+    struct sigaction sa;
+    int i;
+    
+    sa.sa_handler = ack_handler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+    
+    i = 0;
+    while (i < 8)
+    {
+        g_ack_received = 0;
+        
+        if (c & (1 << i))
+            kill(server_pid, SIGUSR1);
+        else
+            kill(server_pid, SIGUSR2);
+        
+        while (!g_ack_received)
+            pause();
+        i++;
+    }
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int i = 0;
-	if (argc == 3)
-	{
-		int server_id;
-		server_id = ft_atoi(argv[1]);
-		while (argv[2][i])
-		{
-			send_char(server_id, argv[2][i]);
-			i++;
-		}
-	}
-	else
-		write(1, "The number of arguments must be three!", 39);
+    int i = 0;
+    int server_pid;
+    
+    if (argc == 3)
+    {
+        server_pid = ft_atoi(argv[1]);
+        if (server_pid <= 0)
+        {
+            write(1, "Invalid server PID!\n", 20);
+            return (1);
+        }
+        
+        while (argv[2][i])
+        {
+            send_char(server_pid, argv[2][i]);
+            i++;
+        }
+        send_char(server_pid, '\0');
+        
+        return (0);
+    }
+    else
+    {
+        write(1, "Usage: ./client [server_pid] [message]\n", 39);
+        return (1);
+    }
 }
